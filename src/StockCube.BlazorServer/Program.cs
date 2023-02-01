35c61510906bb9072af80f5d9066f23f6b5032d4
Kian Abrahams <35241449+KianAbrahams@users.noreply.github.com>
2023-02-01 19:40:53 +0000

    Remove Tailwind and Add MudBlazor

--- src/StockCube.BlazorServer/Program.cs
+++ src/StockCube.BlazorServer/Program.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using StockCube.UI;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
 
 builder.Services.AddStockCubeDomainModel();
 builder.Services.AddStockCubeInfrastructure();
