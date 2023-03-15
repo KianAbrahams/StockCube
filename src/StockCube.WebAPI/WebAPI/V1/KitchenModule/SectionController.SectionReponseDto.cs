17a0d0c0c6fa4c3d4ed1c904b0e4c29ac451a505
Kian Abrahams <35241449+KianAbrahams@users.noreply.github.com>
2023-03-15 20:51:02 +0000

    code cleanup

--- src/StockCube.WebAPI/WebAPI/V1/KitchenModule/SectionController.SectionReponseDto.cs
+++ src/StockCube.WebAPI/WebAPI/V1/KitchenModule/SectionController.SectionReponseDto.cs
@@ -1,8 +1,7 @@
-namespace StockCube.WebAPI.WebAPI.V1.KitchenModule
+namespace StockCube.WebAPI.WebAPI.V1.KitchenModule;
+
+public sealed record SectionResponseDto
 {
-    public sealed record SectionResponseDto
-    {
-        public string Name { get; set; } = string.Empty;
-        public Guid Id { get; set; } = Guid.Empty;
-    }
+    public string Name { get; set; } = string.Empty;
+    public Guid Id { get; set; } = Guid.Empty;
 }
